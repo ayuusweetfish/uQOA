afe418618efec97415237c5452e2fcbfa18b42ae
Ayu <claire@ayu.land>
2023-09-23 22:52:59 +0800

    Reduce internal subroutine exposure

--- main.c
+++ main.c
@@ -8,20 +8,28 @@ int main()
 {
   uint32_t seed = 20230923;
 
+  qoa_lms lms = { 0 };
+  uint8_t sf = 0;
+
   for (int block = 0; block < 3; block++) {
+    puts("------");
     int16_t samples[20];
     for (int i = 0; i < 20; i++) {
       seed = (seed * 1103515245 + 12345);
-      samples[i] = (int16_t)floorf(sinf(i * 0.9f) * 32767.5f) + (seed % 1024);
+      samples[i] = (int16_t)floorf(sinf((i + 20 * block) * 0.9f) * 32767.5f) + (seed % 1024);
       printf("%6d%c", samples[i], i == 19 ? '\n' : ' ');
     }
 
-    qoa_lms lms = { 0 };
-    uint8_t sf = 0;
+    qoa_lms lms_start = lms;
+    for (int i = 0; i < 4; i++) printf("%04hx", lms.history[i]);
+    for (int i = 0; i < 4; i++) printf("%04hx", lms.weights[i]);
+    putchar(' ');
+
+    sf = 0;
     uint64_t slice = qoa_encode_slice(&lms, samples, &sf);
     printf("%016llx\n", slice);
 
-    lms = (qoa_lms){ 0 };
+    lms = lms_start;
     qoa_decode_slice(&lms, slice, samples);
     for (int i = 0; i < 20; i++)
       printf("%6d%c", samples[i], i == 19 ? '\n' : ' ');
--- uqoa.h
+++ uqoa.h
@@ -33,43 +33,53 @@ static void qoa_lms_update(qoa_lms *lms, int16_t sample, int16_t residual)
   lms->history[QOA_LMS_LEN - 1] = sample;
 }
 
-static const int16_t qoa_scalefactor_tab[16] = {
+// scalefactor_tab[s] = round(pow(s + 1, 2.75))
+// Unused throughout library code
+static const uint16_t qoa_scalefactor_tab[16] = {
   1, 7, 21, 45, 84, 138, 211, 304, 421, 562, 731, 928, 1157, 1419, 1715, 2048
 };
-static const int32_t qoa_reciprocal_tab[16] = {
-  65536, 9363, 3121, 1457, 781, 475, 311, 216, 156, 117, 90, 71, 57, 47, 39, 32
-};
-static const int16_t qoa_dequant_tab[16][8] = {
-  {   1,    -1,    3,    -3,    5,    -5,     7,     -7},
-  {   5,    -5,   18,   -18,   32,   -32,    49,    -49},
-  {  16,   -16,   53,   -53,   95,   -95,   147,   -147},
-  {  34,   -34,  113,  -113,  203,  -203,   315,   -315},
-  {  63,   -63,  210,  -210,  378,  -378,   588,   -588},
-  { 104,  -104,  345,  -345,  621,  -621,   966,   -966},
-  { 158,  -158,  528,  -528,  950,  -950,  1477,  -1477},
-  { 228,  -228,  760,  -760, 1368, -1368,  2128,  -2128},
-  { 316,  -316, 1053, -1053, 1895, -1895,  2947,  -2947},
-  { 422,  -422, 1405, -1405, 2529, -2529,  3934,  -3934},
-  { 548,  -548, 1828, -1828, 3290, -3290,  5117,  -5117},
-  { 696,  -696, 2320, -2320, 4176, -4176,  6496,  -6496},
-  { 868,  -868, 2893, -2893, 5207, -5207,  8099,  -8099},
-  {1064, -1064, 3548, -3548, 6386, -6386,  9933,  -9933},
-  {1286, -1286, 4288, -4288, 7718, -7718, 12005, -12005},
-  {1536, -1536, 5120, -5120, 9216, -9216, 14336, -14336},
-};
-static const uint8_t qoa_quant_tab[17] = {
-  7, 7, 7, 5, 5, 3, 3, 1, /* -8..-1 */
-  0,                      /*  0     */
-  0, 2, 2, 4, 4, 6, 6, 6  /*  1.. 8 */
+// dqt = {0.75, 2.5, 4.5, 7}
+// qoa_dequant_tab[s][q] = round_away_0(scalefactor_tab[s] * dqt[q])
+static const int16_t qoa_dequant_tab[16][4] = {
+  {   1,    3,    5,     7},
+  {   5,   18,   32,    49},
+  {  16,   53,   95,   147},
+  {  34,  113,  203,   315},
+  {  63,  210,  378,   588},
+  { 104,  345,  621,   966},
+  { 158,  528,  950,  1477},
+  { 228,  760, 1368,  2128},
+  { 316, 1053, 1895,  2947},
+  { 422, 1405, 2529,  3934},
+  { 548, 1828, 3290,  5117},
+  { 696, 2320, 4176,  6496},
+  { 868, 2893, 5207,  8099},
+  {1064, 3548, 6386,  9933},
+  {1286, 4288, 7718, 12005},
+  {1536, 5120, 9216, 14336},
 };
 
-static inline int16_t qoa_div(int32_t v, int16_t scalefactor) {
-  int32_t reciprocal = qoa_reciprocal_tab[scalefactor];
-  int16_t n = (v * reciprocal + (1 << 15)) >> 16;
-  n = n + ((v > 0) - (v < 0)) - ((n > 0) - (n < 0)); /* round away from 0 */
-  return n;
+static inline int16_t qoa_quant(int32_t v, int16_t sf)
+{
+  // |v| <= 65535
+  // Calculate n = v / qoa_scalefactor_tab[sf]
+  // reciprocal[s] = ceil(32768 / scalefactor_tab[s])
+  static const uint16_t reciprocal[16] = {
+    32768, 4682, 1561, 729, 391, 238, 156, 108, 78, 59, 45, 36, 29, 24, 20, 16
+  };
+  int16_t n = (v * reciprocal[sf] + (1 << 14)) >> 15;
+  n = n + ((v > 0) - (v < 0)) - ((n > 0) - (n < 0));  // Round away from 0
+
+  // Clamp into [-8, 8]
+  if (n < -8) n = -8;
+  if (n >  8) n = 8;
+  static const uint8_t quant[17] = {
+    7, 7, 7, 5, 5, 3, 3, 1, // -8..-1
+    0,                      //  0
+    0, 2, 2, 4, 4, 6, 6, 6  //  1.. 8
+  };
+  return quant[8 + n];
 }
-#define qoa_clamp(_x, _l, _h) ((_x) < (_l) ? (_l) : (_x) > (_h) ? (_h) : (_x))
 
 static inline int16_t qoa_sat_s16(int32_t x)
 {
@@ -80,11 +90,14 @@ static inline int16_t qoa_sat_s16(int32_t x)
 
 void qoa_start_frame(qoa_lms *lms)
 {
+  // Cannot check once at the end due to the edge case
+  // where all four weights equal 32768 and `sq_sum` wraps around to 0
   uint32_t sq_sum = 0;
-  for (int i = 0; i < QOA_LMS_LEN; i++)
+  for (int i = 0; i < QOA_LMS_LEN; i++) {
     sq_sum += (int32_t)lms->weights[i] * lms->weights[i];
-  if (sq_sum > 0x2fffffff)
-    for (int i = 0; i < QOA_LMS_LEN; i++) lms->weights[i] = 0;
+    if (sq_sum > 0x2fffffff)
+      for (int i = 0; i < QOA_LMS_LEN; i++) lms->weights[i] = 0;
+  }
 }
 
 uint64_t qoa_encode_slice(qoa_lms *lms, int16_t samples[20], uint8_t *sf_hint)
@@ -104,13 +117,13 @@ uint64_t qoa_encode_slice(qoa_lms *lms, int16_t samples[20], uint8_t *sf_hint)
       int16_t sample = samples[i];
       int32_t predicted = qoa_lms_predict(&current_lms);
       int32_t residual = sample - predicted;
-      int16_t scaled = qoa_div(residual, sf);
-      int16_t quantized = qoa_quant_tab[8 + qoa_clamp(scaled, -8, 8)];
-      int16_t dequantized = qoa_dequant_tab[sf][quantized];
+      int16_t quantized = qoa_quant(residual, sf);
+      int16_t dequantized = qoa_dequant_tab[sf][quantized / 2];
+      if (quantized % 2 == 1) dequantized = -dequantized;
       int16_t reconstructed = qoa_sat_s16(predicted + dequantized);
 
       int16_t error = sample - reconstructed;
-      // printf("%d %d %d %d\n", (int)sample, (int)quantized, (int)reconstructed, (int)error);
+      // printf("%2d %2d %6d %6d %1d %6d %d\n", sf, i, (int)sample, (int)predicted, (int)quantized, (int)reconstructed, (int)error);
       current_error += (int32_t)error * error;
       if (current_error > best_error) break;
 
@@ -138,7 +151,8 @@ void qoa_decode_slice(qoa_lms *lms, uint64_t slice, int16_t out_samples[20])
   for (int i = 0; i < 20; i++) {
     int32_t predicted = qoa_lms_predict(lms);
     int16_t quantized = (slice >> 57) & 0x7;
-    int16_t dequantized = qoa_dequant_tab[sf][quantized];
+    int16_t dequantized = qoa_dequant_tab[sf][quantized / 2];
+    if (quantized % 2 == 1) dequantized = -dequantized;
     int16_t reconstructed = qoa_sat_s16(predicted + dequantized);
     out_samples[i] = reconstructed;
     slice <<= 3;
