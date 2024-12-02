4fcdbd725ff85cce7351b39d347ecc347a17fca1
Ayu <claire@ayu.land>
2024-12-02 14:22:58 +0800

    uqoa_conv: Add plain format option

--- uqoa_conv.c
+++ uqoa_conv.c
@@ -1,6 +1,7 @@
 #define uQOA_IMPL
 #include "uqoa.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
 
@@ -15,6 +16,8 @@
 // [2 .. 257] - slices, each uint64 represents 20 samples
 // The trailing samples are truncated
 // if the total number of samples is not divisible by 20
+
+// If the `-p` (plain) option is supplied, history and weights are omitted
 
 static inline void put_u64(uint64_t x)
 {
@@ -32,7 +35,7 @@ static inline uint8_t silent_getchar()
   return (uint8_t)c;
 }
 
-void encode()
+void encode(bool is_plain)
 {
   qoa_lms lms = { 0 };
   uint8_t sf = 0;
@@ -49,20 +52,23 @@ void encode()
     samples[sample_count++] = sample;
     if (sample_count == 20) {
       if (slice_count == 0) {
-        // New frame, reiterate state
+        // New frame, refresh state to eliminate near-overflows
+        // Save state in data stream, if not plain format
         qoa_start_frame(&lms);
-        put_u64(
-          (uint64_t)(uint16_t)lms.history[0] << 48 |
-          (uint64_t)(uint16_t)lms.history[1] << 32 |
-          (uint64_t)(uint16_t)lms.history[2] << 16 |
-          (uint64_t)(uint16_t)lms.history[3] <<  0
-        );
-        put_u64(
-          (uint64_t)(uint16_t)lms.weights[0] << 48 |
-          (uint64_t)(uint16_t)lms.weights[1] << 32 |
-          (uint64_t)(uint16_t)lms.weights[2] << 16 |
-          (uint64_t)(uint16_t)lms.weights[3] <<  0
-        );
+        if (!is_plain) {
+          put_u64(
+            (uint64_t)(uint16_t)lms.history[0] << 48 |
+            (uint64_t)(uint16_t)lms.history[1] << 32 |
+            (uint64_t)(uint16_t)lms.history[2] << 16 |
+            (uint64_t)(uint16_t)lms.history[3] <<  0
+          );
+          put_u64(
+            (uint64_t)(uint16_t)lms.weights[0] << 48 |
+            (uint64_t)(uint16_t)lms.weights[1] << 32 |
+            (uint64_t)(uint16_t)lms.weights[2] << 16 |
+            (uint64_t)(uint16_t)lms.weights[3] <<  0
+          );
+        }
       }
       uint64_t slice = qoa_encode_slice(&lms, samples, &sf);
       put_u64(slice);
@@ -73,7 +79,7 @@ void encode()
 }
 
 // Decode
-// ./uqoa_conv d < $NAME.bin | ffmpeg -ar 44100 -ac 1 -f s16be -acodec pcm_s16be -i - $NAME.re.wav
+// ./uqoa_conv -d < $NAME.bin | ffmpeg -ar 44100 -ac 1 -f s16be -acodec pcm_s16be -i - $NAME.re.wav
 
 static inline uint64_t get_u64()
 {
@@ -83,17 +89,23 @@ static inline uint64_t get_u64()
   return x;
 }
 
-void decode()
+void decode(bool is_plain)
 {
-  qoa_lms lms;
+  qoa_lms lms = { 0 };
 
   while (!eof) {
-    uint64_t history = get_u64();
-    for (int i = 0; i < 4; i++)
-      lms.history[i] = (int16_t)((history >> ((3 - i) * 16)) & 0xFFFF);
-    uint64_t weights = get_u64();
-    for (int i = 0; i < 4; i++)
-      lms.weights[i] = (int16_t)((weights >> ((3 - i) * 16)) & 0xFFFF);
+    if (!is_plain) {
+      // Load state from data stream
+      uint64_t history = get_u64();
+      for (int i = 0; i < 4; i++)
+        lms.history[i] = (int16_t)((history >> ((3 - i) * 16)) & 0xFFFF);
+      uint64_t weights = get_u64();
+      for (int i = 0; i < 4; i++)
+        lms.weights[i] = (int16_t)((weights >> ((3 - i) * 16)) & 0xFFFF);
+    } else {
+      // Simply reset near-overflows if any
+      qoa_start_frame(&lms);
+    }
     for (int n_slice = 0; n_slice < 256 && !feof(stdin); n_slice++) {
       uint64_t slice = get_u64();
       if (eof) break;
@@ -109,6 +121,21 @@ void decode()
 
 int main(int argc, char *argv[])
 {
-  if (argc >= 2 && argv[1][0] == 'd') decode(); else encode();
+  bool is_plain = false;
+  bool is_decode = false;
+  for (int i = 1; i < argc; i++)
+    if (argv[i][0] == '-') switch (argv[i][1]) {
+      case 'p': is_plain = true; break;
+      case 'd': is_decode = true; break;
+      case 'h':
+        fprintf(stderr, "Usage: %s [-d] [-p]\n"
+          "  -d  Decode (otherwise, encode)\n"
+          "  -p  Plain (do not periodically dump codec state)\n"
+          , argv[0]);
+        return 0;
+    }
+
+  (is_decode ? decode : encode)(is_plain);
+
   return 0;
 }
